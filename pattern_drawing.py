f4004b27531f6c1174d832ecff38c782e14bfe15
Nambuya <hawanambuya@gmail.com>
2025-07-18 15:36:42 +0300

    Add user input and pattern drawing

--- pattern_drawing.py
+++ pattern_drawing.py
@@ -1,16 +1,4 @@
-while True:
-    try:
-        size = int(input("Enter the size of the pattern: "))
-        if size > 0:
-            break
-        else:
-            print("Please enter a positive integer.")
-    except ValueError:
-        print("Invalid input. Please enter a valid integer.")
+size = int(input("Enter the size of the pattern: "))
 
-row = 0
-while row < size:
-    for _ in range(size):
-        print("*", end="")
-    print()  
-    row += 1
+for i in range(1, size + 1):
+    print('*' * i)
