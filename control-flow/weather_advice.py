edb35f06eaccccacea32f873e13e5e68931d155d
Nambuya <hawanambuya@gmail.com>
2025-07-18 11:23:52 +0300

    Fix: exact if sunny line for checker

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,13 +1,7 @@
 weather = input("What's the weather like today? (sunny/rainy/cold): ")
-sunny
-if weather == "sunny:"
-   print("Wear a t-shirt and sunglasses.") 
-rainy   
-elif weather == "rainy":
-     print("Dont forget your umbrella and a raincoat.")
-cold
-elif weather == "cold":
-     print("Make sure to wear a warm coat and a scarf.")
-else:
-    print("Sorry, i dont have recommendations for this weather.")
+if weather == "sunny":print("Wear a t-shirt and sunglasses.")     
+elif weather == "rainy":print("Dont forget your umbrella and a raincoat.")
+elif weather == "cold":print("Make sure to wear a warm coat and a scarf.")
+else: print("Sorry, i dont have recommendations for this weather.")
+    
        
