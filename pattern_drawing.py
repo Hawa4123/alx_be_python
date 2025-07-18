ad85075681a15dea37fd73d9155fcf4ef698a5bd
Nambuya <hawanambuya@gmail.com>
2025-07-18 16:10:43 +0300

    Fix input format and meet checker requirements

--- pattern_drawing.py
+++ pattern_drawing.py
@@ -1,6 +1,4 @@
-while True:
-    try:
-        size = int(input("Enter the size of the pattern: "))
+  size = int(input("Enter the size of the pattern: "))
         if size <= 0:
             print("Please enter a positive integer greater than zero.")
             continue
