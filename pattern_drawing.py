2dba069180b0a811f00d904b9e8230df3d5f11e6
Nambuya <hawanambuya@gmail.com>
2025-07-18 16:07:14 +0300

    Add pattern drawing script with input validation

--- pattern_drawing.py
+++ pattern_drawing.py
@@ -1,4 +1,16 @@
-size = int(input("Enter the size of the pattern: "))
+while True:
+    try:
+        size = int(input("Enter the size of the pattern: "))
+        if size <= 0:
+            print("Please enter a positive integer greater than zero.")
+            continue
+        break
+    except ValueError:
+        print("Please enter a valid positive integer.")
 
-for i in range(1, size + 1):
-    print('*' * i)
+row = 0
+while row < size:
+    for _ in range(size):
+        print("*", end="")
+    print()
+    row += 1
